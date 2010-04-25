bdd00f9bc3d420291da44276c2adc1fb6ed18715
Jonah Burke <jonah@jonahb.com>
2010-04-25 16:37:45 -0400

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,46 @@
+require 'rubygems'
+require 'rubygems/specification'
+require 'rake/testtask'
+require 'rake/packagetask'
+require 'rake/gempackagetask'
+require 'lib/akismet'
+
+spec = Gem::Specification.new do |s|
+  s.name    = "akismet"
+  s.platform = Gem::Platform::RUBY
+  s.version = Akismet::VERSION
+  s.authors = ["Jonah Burke"]
+  s.email = ["jonah@bigthink.com"]
+  s.description = s.summary = "A Ruby client for the Akismet API"
+  s.required_rubygems_version = ">= 1.3.5"
+  s.require_path = 'lib'
+  s.files = Dir.glob( "lib/**/*" )
+  # s.homepage = ""
+  # s.has_rdoc = true
+  # s.extra_rdoc_files = ["README.markdown", "LICENSE"]
+end
+
+Rake::TestTask.new do |t|
+  t.libs << "test"
+  t.pattern = 'test/*_test.rb'
+  t.verbose = true
+  t.warning = false
+end
+
+Rake::GemPackageTask.new(spec) do |p|
+  p.gem_spec = spec
+end
+
+desc "install the gem locally"
+task :install => [:package] do
+  sh %{gem install pkg/#{spec.name}-#{spec.version}}
+end
+
+desc "create a gemspec file"
+task :make_spec do
+  File.open("#{spec.name}.gemspec", "w") do |file|
+    file.puts spec.to_ruby
+  end
+end
+
+task :gem => :make_spec
---
+++ test/akismet_test.rb
@@ -0,0 +1,58 @@
+require 'test/unit'
+require File.dirname( __FILE__ ) + '/../lib/akismet'
+
+class AkismetTest < Test::Unit::TestCase
+  def setup
+    # jonahb2's personal key
+    @key = 'ecd2022d3247'
+    @home_url = 'http://example.com'
+  end
+
+  def test_verify_key_fails_with_invalid_key
+    assert !Akismet.verify_key( 'bad_key', '' )
+  end
+  
+  def test_verify_key_fails_with_invalid_home_url
+    assert !Akismet.verify_key( @key, '' )
+  end
+  
+  def test_verify_key_succeeds
+    assert Akismet.verify_key( @key, @home_url )
+  end
+
+  def test_check_comment_returns_true_for_spam
+    # the author 'viagra-test-123' always triggers a true response
+    assert Akismet.comment_check( @key,
+      @home_url,
+      '123.123.123.123',
+      'Test user-agent',
+      :permalink => 'http://example.com',
+      :comment_type => Akismet::CommentType::COMMENT,
+      :comment_author => 'viagra-test-123',
+      :comment_author_email => 'test@example.com',
+      :comment_author_url => 'http://example.com',
+      :comment_content => 'Here is the content' ) 
+  end
+
+  def test_check_comment_returns_true_for_spam_with_minimal_parameters
+    assert Akismet.comment_check( @key,
+      @home_url,
+      '123.123.123.123',
+      'Test user-agent',
+      :comment_author => 'viagra-test-123' )
+  end
+
+  def test_submit_ham
+    assert_nil Akismet.submit_ham( @key,
+      @home_url,
+      '123.123.123.123',
+      'Test user-agent' )
+  end
+
+  def test_submit_spam
+    assert_nil Akismet.submit_ham( @key,
+      @home_url,
+      '123.123.123.123',
+      'Test user-agent' )
+  end
+end
